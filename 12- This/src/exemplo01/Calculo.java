dc6ab2c2c633f321cf141d9491a6b7d6ea0b5d47
VitorBM <39385672+VitorBM@users.noreply.github.com>
2018-05-18 17:26:11 -0300

    12 - This

---
+++ 12- This/src/exemplo01/Calculo.java
@@ -0,0 +1,19 @@
+package exemplo01;
+
+public class Calculo {
+	
+	//Atributo da classe
+	public double resultado = 10;
+	
+	//Método para exibir um resultado
+	public void resultado(){
+		
+		//Variável local
+		double resultado = 5;
+		
+		//Exibir o resultado
+		System.out.println(this.resultado);
+		
+	}
+
+}
